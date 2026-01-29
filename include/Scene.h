1d98ccf134a43474cddc79ec84029cf6f6391c37
piotrkrysik <147055503+piotrkrysik@users.noreply.github.com>
2026-01-29 21:09:41 +0100

    poprawka

--- include/Scene.h
+++ include/Scene.h
@@ -9,12 +9,12 @@ public:
     Scene(GameEngine* engine) : m_engine(engine) {}
     virtual ~Scene() = default;
 
-    virtual void onActivate() {}      // wywo³ywane przy wejœciu do sceny
-    virtual void onDeactivate() {}    // przy wyjœciu
+    virtual void onActivate() {}
+    virtual void onDeactivate() {}
     virtual void sProcessInput() = 0;
     virtual void sUpdate(float dt) = 0;
     virtual void sRender() = 0;
 
 protected:
-    GameEngine* m_engine;  // pozwala scenie korzystaæ z zasobów, zmieniaæ sceny itd.
+    GameEngine* m_engine;
 };
