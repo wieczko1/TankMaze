71f26f1714a6321ff10e1414503457c343506e29
piotrkrysik <147055503+piotrkrysik@users.noreply.github.com>
2026-01-29 21:10:44 +0100

    poprawka

--- include/TileMapManager.h
+++ include/TileMapManager.h
@@ -9,7 +9,6 @@ private:
     TextureManager& m_texMgr;
     float m_tileSize;
     sf::VertexArray m_vertices;
-    // Bêdziemy potrzebowaæ te¿ listy Bounds dla kolizji, skoro nie mamy Sprite'ów
     std::vector<sf::FloatRect> m_wallBounds;
 
 public:
@@ -17,6 +16,5 @@ public:
     void loadMap(const std::vector<std::string>& levelData);
     void draw(sf::RenderWindow& window);
 
-    // Zamiast wektora Sprite'ów, zwracamy wektor prostok¹tów kolizji
     const std::vector<sf::FloatRect>& getWallBounds() const;
 };
